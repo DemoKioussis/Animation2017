ddbd6396ed870c644931f7301852cc45ad9e30bc
Demo Kioussis <Demo.kioussis@gmail.com>
2017-09-06 21:59:59 -0400

    testing

--- Animation2017/Main.cpp
+++ Animation2017/Main.cpp
@@ -15,6 +15,7 @@ const unsigned int SCR_HEIGHT = 600;
 int main()
 {
 
+
 	glm::vec3 test(1,2,3);
 	// glfw: initialize and configure
 	// ------------------------------
@@ -57,7 +58,7 @@ int main()
 
 		// render
 		// ------
-		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClearColor(1.0f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
